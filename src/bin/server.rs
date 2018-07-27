022dc991ceec793f068dad334f07282ebe3c68bc
Longor1996 <lalongok@gmail.com>
2018-07-27 22:54:16 +0200

    Implemented streaming UTF-8 decoder for #1

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,27 @@
 #[macro_use] extern crate failure;
 extern crate TCGE;
 
+use TCGE::resources::Resources;
+use TCGE::util::utf8;
+use TCGE::util::cdml;
+
 fn main() {
+    let res = Resources::from_exe_path().unwrap();
+
     println!("Hello, Server! {}", TCGE::MAGIC);
+
+    let mut reader = res.open("test.cdml").unwrap();
+    let mut reader= utf8::UTF8Read::new(reader);
+
+    print!("\n");
+    loop {
+        match reader.read() {
+            Ok(x) => print!("{}", x),
+            Err(e) => break
+        }
+    }
+    print!("\n");
+
+    // let mut cdmlReader = cdml::CDMLReader::read(reader);
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub const MAGIC : u32 = 42;
 pub mod resources;
 pub mod client;
 pub mod server;
+pub mod util;
