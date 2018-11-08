7d31be8bd032ef7da5b64d6a05ff96c97d53cdc2
Longor1996 <lalongok@gmail.com>
2018-11-08 21:52:17 +0100

    Implemented basic Camera
    Issue #9; First-person freecam and refactoring.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,20 @@
+use std::rc::Rc;
+use std::cell::Ref;
+use std::cell::RefMut;
+use std::cell::RefCell;
+
 extern crate failure;
-extern crate TCGE;
+use failure::Fail;
+
 extern crate time;
 extern crate cgmath;
+
 extern crate glfw;
 use self::glfw::{Context, Key, Action};
 
 extern crate gl;
 
+extern crate TCGE;
 use TCGE::resources::Resources;
 use TCGE::client::render_gl;
 use TCGE::gameloop;
@@ -63,6 +71,8 @@ fn run() -> Result<(), failure::Error> {
 
     window.make_current();
     window.set_key_polling(true);
+    window.set_cursor_pos_polling(true);
+    window.set_cursor_mode(glfw::CursorMode::Disabled);
     window.set_framebuffer_size_polling(true);
     window.set_size_limits(
         320, 225,
@@ -81,37 +91,66 @@ fn run() -> Result<(), failure::Error> {
     shader_program.set_used();
 
     // ------------------------------------------
-    let geometry = geometry_test();
-
-    let mut render_state = RenderState {
+    let render_state = RenderState {
         uniform_mat: shader_program.uniform_location("transform"),
         uniform_time: shader_program.uniform_location("time"),
-        shader_program: shader_program,
-        geometry: geometry
+        shader_program: shader_program
     };
 
-    let camera = Camera {
-        position: cgmath::Vector3 {x: 0.0, y: 0.0, z: 0.0},
-        velocity: cgmath::Vector3 {x: 0.0, y: 0.0, z: 0.0},
-        rotation: cgmath::Vector2 {x: 0.0, y: 0.0}
-    };
+    let mut cursor = Cursor {pos_x: 0.0, pos_y: 0.0, mov_x: 0.0, mov_y: 0.0};
 
-    let mut gls = gameloop::newGameloop(20);
+    let scene = Rc::new(RefCell::new(Option::Some(Scene {
+        camera: Camera {
+            position: cgmath::Vector3 {x: 0.0, y: 1.8, z: 0.0},
+            velocity: cgmath::Vector3 {x: 0.0, y: 0.0, z: 0.0},
+            rotation: cgmath::Vector2 {x: 0.0, y: 90.0}
+        },
+        meshes: vec![geometry_test()]
+    })));
 
     // ------------------------------------------
+    let mut gls = gameloop::newGameloop(20);
+
     while !window.should_close() {
-        process_events(&mut window, &events);
+        process_events(
+            &mut window,
+            &events,
+            &mut cursor,
+            &mut *scene.borrow_mut()
+        );
+
+        let window_size = window.get_framebuffer_size();
 
         gameloop::gameloop_next(&mut gls,
             || {glfw.get_time()},
+
             |now:f64| {
-                println!("It is now {}", now);
+                // println!("It is now {}", now);
+
+                scene.borrow().as_ref().map(|scene| {
+                    println!("CAMERA {}", scene.camera);
+                });
+
+                scene.borrow_mut().as_mut().map(|mut_scene| {
+                    mut_scene.camera.update_movement(&window);
+                });
             },
+
             |now:f64, interpolation:f32| {
-                render(&render_state, &camera, now);
+                scene.borrow().as_ref().map(|scene| {
+                    render(
+                        &render_state,
+                        &scene,
+                        &scene.camera,
+                        window_size,
+                        now,
+                        interpolation
+                    )
+                });
+
             }
         );
-        
+
         window.swap_buffers();
         glfw.poll_events();
     }
@@ -119,34 +158,63 @@ fn run() -> Result<(), failure::Error> {
     Ok(())
 }
 
+struct Scene {
+    camera: Camera,
+    meshes: Vec<SimpleVAO>,
+}
+
+struct Cursor {
+    pos_x: f32,
+    pos_y: f32,
+    mov_x: f32,
+    mov_y: f32,
+}
+
+impl Cursor {
+    fn update(&mut self, x: f64, y: f64) {
+        self.mov_x = (x as f32) - self.pos_x;
+        self.mov_y = (y as f32) - self.pos_y;
+        self.pos_x = x as f32;
+        self.pos_y = y as f32;
+    }
+}
+
 struct RenderState {
     uniform_mat: i32,
     uniform_time: i32,
-    shader_program: render_gl::Program,
-    geometry: SimpleVAO,
+    shader_program: render_gl::Program
 }
 
-fn render(render_state: &RenderState, camera: &Camera, now: f64) {
+fn render(render_state: &RenderState, scene: &Scene, camera: &Camera, size: (i32, i32), now: f64, interpolation:f32) {
     unsafe {
         gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        gl::Enable(gl::DEPTH_TEST);
+        gl::CullFace(gl::FRONT);
+        gl::Enable(gl::CULL_FACE);
     }
 
     render_state.shader_program.set_used();
-    render_state.shader_program.uniform_matrix4(render_state.uniform_mat, camera.transform());
+    render_state.shader_program.uniform_matrix4(render_state.uniform_mat, camera.transform(size));
     render_state.shader_program.uniform_scalar(render_state.uniform_time, now as f32);
-    unsafe {
-        gl::BindVertexArray(render_state.geometry.handle);
-        gl::DrawArrays(
-            gl::TRIANGLES,
-            0, render_state.geometry.count
-        );
+
+    for mesh in scene.meshes.iter() {
+        unsafe {
+            gl::BindVertexArray(mesh.handle);
+            gl::DrawArrays(
+                gl::TRIANGLES,
+                0, mesh.count
+            );
+        }
     }
 }
 
 use std::sync::mpsc::Receiver;
-use failure::Fail;
-
-fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+fn process_events(
+    window: &mut glfw::Window,
+    events: &Receiver<(f64, glfw::WindowEvent)>,
+    cursor: &mut Cursor,
+    opt_scene: &mut Option<Scene>
+) {
     for(_, event) in glfw::flush_messages(events) {
         match event {
             glfw::WindowEvent::FramebufferSize(width, height) => {
@@ -154,6 +222,17 @@ fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::Windo
             },
             glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
                 window.set_should_close(true)
+            },
+            glfw::WindowEvent::CursorPos(x, y) => {
+                cursor.update(x, y);
+                opt_scene.as_mut()
+                    .map(|mut_scene| &mut mut_scene.camera)
+                    .map( |mut_camera| {
+                        mut_camera.update_rotation(
+                            cursor.mov_x,
+                            cursor.mov_y
+                        );
+                    });
             },
             _ => ()
         }
@@ -166,11 +245,23 @@ struct SimpleVAO {
 }
 
 fn geometry_test() -> SimpleVAO {
-    let vertices: Vec<f32> = vec![
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0
+    let mut vertices: Vec<f32> = vec![
+        -0.5, -0.5, -10.0,
+        0.5, -0.5, -10.0,
+        0.0, 0.5, -10.0
     ];
+
+    vertices.extend(&vec![
+        -20.0, 0.0, -20.0,
+          0.0, 0.0,  20.0,
+         20.0, 0.0, -20.0
+    ]);
+
+    vertices.extend(&vec![
+        -5.0, 0.0, 30.0,
+         0.0, 9.0, 30.0,
+         5.0, 0.0, 30.0
+    ]);
 
     let mut vbo: gl::types::GLuint = 0;
     unsafe {
@@ -210,6 +301,8 @@ fn geometry_test() -> SimpleVAO {
     }
 }
 
+// TODO: Camera needs PlayerController/ClientInput...
+#[derive(Debug)]
 struct Camera {
     position: cgmath::Vector3<f32>,
     velocity: cgmath::Vector3<f32>,
@@ -217,14 +310,109 @@ struct Camera {
 }
 
 impl Camera {
-    fn transform(&self) -> cgmath::Matrix4<f32> {
-        let mat = cgmath::Matrix4::new(
+    fn transform(&self, size: (i32,i32) ) -> cgmath::Matrix4<f32> {
+        use cgmath::Matrix4;
+
+        let (width, height) = size;
+        let fov = cgmath::Rad::from(cgmath::Deg(90.0));
+
+        let perspective = cgmath::PerspectiveFov {
+            fovy: fov,
+            aspect: width as f32 / height as f32,
+            near: 0.1, far: 1024.0
+        };
+
+        let perspective = Matrix4::from(perspective);
+
+        // this next section can most certainly be written with less code...
+        let mut camera = Matrix4::new(
             1.0, 0.0, 0.0, 0.0,
             0.0, 1.0, 0.0, 0.0,
             0.0, 0.0, 1.0, 0.0,
             0.0, 0.0, 0.0, 1.0
         );
 
-        return mat
+        let pitch = cgmath::Deg(self.rotation.x);
+        let yaw = cgmath::Deg(self.rotation.y);
+
+        camera = camera * Matrix4::from_angle_x(pitch);
+        camera = camera * Matrix4::from_angle_y(yaw);
+        camera = camera * Matrix4::from_nonuniform_scale(1.0,1.0,-1.0);
+        camera = camera * Matrix4::from_translation(-self.position);
+
+        return perspective * camera;
+    }
+
+    fn update_rotation(&mut self, yaw: f32, pitch: f32) {
+        let mouse_sensivity = 0.5;
+
+        self.rotation.x += pitch * mouse_sensivity;
+        if self.rotation.x < -90.0 {
+            self.rotation.x = -90.0;
+        }
+        if self.rotation.x > 90.0 {
+            self.rotation.x = 90.0;
+        }
+
+        self.rotation.y += yaw * mouse_sensivity;
+        while self.rotation.y < 0.0 {
+            self.rotation.y += 360.0;
+        }
+        while self.rotation.y > 360.0 {
+            self.rotation.y -= 360.0;
+        }
+    }
+
+    fn update_movement(&mut self, window: & glfw::Window) {
+        use cgmath::Vector3;
+        use cgmath::Matrix4;
+        use cgmath::Transform;
+
+        let move_speed = 0.5;
+
+        if window.get_key(Key::LeftShift) == Action::Press {
+            self.position += Vector3::new(0.0, -1.0, 0.0) * move_speed;
+        }
+        if window.get_key(Key::Space) == Action::Press {
+            self.position += Vector3::new(0.0, 1.0, 0.0) * move_speed;
+        }
+
+        let yaw = cgmath::Deg(self.rotation.y);
+        let mat = Matrix4::from_angle_y(yaw);
+
+        let forward = Vector3::new(0.0, 0.0, 1.0);
+        let forward = Matrix4::transform_vector(&mat, forward);
+        if window.get_key(Key::W) == Action::Press {
+            self.position += forward * move_speed;
+        }
+
+        let backward = Vector3::new(0.0, 0.0, -1.0);
+        let backward = Matrix4::transform_vector(&mat, backward);
+        if window.get_key(Key::S) == Action::Press {
+            self.position += backward * move_speed;
+        }
+
+        let left = Vector3::new(-1.0, 0.0, 0.0);
+        let left = Matrix4::transform_vector(&mat, left);
+        if window.get_key(Key::A) == Action::Press {
+            self.position += left * move_speed;
+        }
+
+        let right = Vector3::new(1.0, 0.0, 0.0);
+        let right = Matrix4::transform_vector(&mat, right);
+        if window.get_key(Key::D) == Action::Press {
+            self.position += right * move_speed;
+        }
+    }
+}
+
+impl std::fmt::Display for Camera {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "Camera [x: {}, z: {}, pitch: {}, yaw: {} ]",
+               self.position.x,
+               self.position.z,
+               self.rotation.x,
+               self.rotation.y
+        )
     }
 }
