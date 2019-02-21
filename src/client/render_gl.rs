3d01e9686e9639995120ff59e7e996d95986a72e
Longor1996 <lalongok@gmail.com>
2019-02-21 18:31:07 +0100

    Formatting and UV's

--- src/client/render_gl.rs
+++ src/client/render_gl.rs
@@ -9,295 +9,312 @@ use client::render_gl::cgmath::prelude::*;
 
 #[derive(Debug, Fail)]
 pub enum Error {
-    #[fail(display = "Failed to load resource {}", name)]
-    ResourceLoad { name: String, #[cause] inner: resources::ResError },
-
-    #[fail(display = "Can not determine shader type for resource {}", name)]
-    CanNotDetermineShaderTypeForResource { name: String },
-
-    #[fail(display = "Failed to compile shader {}: {}", name, message)]
-    CompileError { name: String, message: String },
-
-    #[fail(display = "Failed to link program {}: {}", name, message)]
-    LinkError { name: String, message: String },
-    
-    #[fail(display = "Failed to parse image {}", name)]
-    ImageParse { name: String, #[cause] inner: image::ImageError },
+	#[fail(display = "Failed to load resource {}", name)]
+	ResourceLoad { name: String, #[cause] inner: resources::ResError },
+	
+	#[fail(display = "Can not determine shader type for resource {}", name)]
+	CanNotDetermineShaderTypeForResource { name: String },
+	
+	#[fail(display = "Failed to compile shader {}: {}", name, message)]
+	CompileError { name: String, message: String },
+	
+	#[fail(display = "Failed to link program {}: {}", name, message)]
+	LinkError { name: String, message: String },
+	
+	#[fail(display = "Failed to parse image {}", name)]
+	ImageParse { name: String, #[cause] inner: image::ImageError },
 }
 
 pub struct Program {
-    id: gl::types::GLuint,
+	id: gl::types::GLuint,
 }
 
 impl Program {
-    pub fn from_res(res: &Resources, name: &str) -> Result<Program, Error> {
-        const POSSIBLE_EXT: [&str; 2] = [
-            ".vert",
-            ".frag",
-        ];
-        
-        let resource_names = POSSIBLE_EXT.iter()
-            .map(|file_extension| format!("{}{}", name, file_extension))
-            .collect::<Vec<String>>();
-    
-        println!("Loading program shaders: {}", name);
-        let shaders = resource_names.iter()
-            .map(|resource_name| {
-                Shader::from_res(res, resource_name)
-            })
-            .collect::<Result<Vec<Shader>, Error>>()?;
-    
-        println!("Compiling program: {}", name);
-        Program::from_shaders(&shaders[..])
-            .map_err(|message| Error::LinkError { name: name.into(), message })
-    }
-
-    pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
-        let program_id = unsafe { gl::CreateProgram() };
-
-        for shader in shaders {
-            unsafe { gl::AttachShader(program_id, shader.id()); }
-        }
-
-        unsafe { gl::LinkProgram(program_id); }
-
-        let mut success: gl::types::GLint = 1;
-        unsafe {
-            gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
-        }
-
-        if success == 0 {
-            let mut len: gl::types::GLint = 0;
-            unsafe {
-                gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
-            }
-
-            let error = create_whitespace_cstring_with_len(len as usize);
-            unsafe {
-                gl::GetProgramInfoLog(
-                    program_id,
-                    len,
-                    std::ptr::null_mut(),
-                    error.as_ptr() as *mut gl::types::GLchar
-                );
-            }
-
-            return Err(error.to_string_lossy().into_owned());
-        }
-
-        for shader in shaders {
-            unsafe { gl::DetachShader(program_id, shader.id()); }
-        }
-
-        Ok(Program { id: program_id })
-    }
-
-    pub fn id(&self) -> gl::types::GLuint {
-        self.id
-    }
-
-    pub fn set_used(&self) {
-        unsafe {
-            gl::UseProgram(self.id);
-        }
-    }
-
-    pub fn uniform_location(&self, uniform_name: &str) -> i32 {
-        let vstr = uniform_name.as_bytes().to_owned();
-        let cstr = unsafe { CString::from_vec_unchecked(vstr) };
-        unsafe {
-            let loc = gl::GetUniformLocation(self.id, cstr.as_ptr());
-
-            if loc == -1 {
-                eprintln!("Uniform location for '{}' is invalid.", uniform_name);
-                return 0;
-            }
-
-            loc
-        }
-    }
-
-    pub fn uniform_scalar(&self, uniform: i32, value: f32) {
-        if uniform == -1 {return}
-        unsafe {
-            gl::Uniform1f(uniform, value)
-        }
-    }
-    
-    pub fn uniform_vector4(&self, uniform: i32, vector: cgmath::Vector4<f32>) {
-        if uniform == -1 {return}
-        unsafe {
-            gl::Uniform4fv(uniform, 1, vector.as_ptr())
-        }
-    }
-
-    pub fn uniform_matrix4(&self, uniform: i32, matrix: cgmath::Matrix4<f32>) {
-        if uniform == -1 {return}
-        unsafe {
-            gl::UniformMatrix4fv(uniform, 1, gl::FALSE, matrix.as_ptr())
-        }
-    }
+	pub fn from_res(res: &Resources, name: &str) -> Result<Program, Error> {
+		const POSSIBLE_EXT: [&str; 2] = [
+			".vert",
+			".frag",
+		];
+		
+		let resource_names = POSSIBLE_EXT.iter()
+			.map(|file_extension| format!("{}{}", name, file_extension))
+			.collect::<Vec<String>>();
+		
+		println!("Loading program shaders: {}", name);
+		let shaders = resource_names.iter()
+			.map(|resource_name| {
+				Shader::from_res(res, resource_name)
+			})
+			.collect::<Result<Vec<Shader>, Error>>()?;
+		
+		println!("Compiling program: {}", name);
+		Program::from_shaders(&shaders[..])
+			.map_err(|message| Error::LinkError { name: name.into(), message })
+	}
+	
+	pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
+		let program_id = unsafe { gl::CreateProgram() };
+		
+		for shader in shaders {
+			unsafe { gl::AttachShader(program_id, shader.id()); }
+		}
+		
+		unsafe { gl::LinkProgram(program_id); }
+		
+		let mut success: gl::types::GLint = 1;
+		unsafe {
+			gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
+		}
+		
+		if success == 0 {
+			let mut len: gl::types::GLint = 0;
+			unsafe {
+				gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
+			}
+			
+			let error = create_whitespace_cstring_with_len(len as usize);
+			unsafe {
+				gl::GetProgramInfoLog(
+					program_id,
+					len,
+					std::ptr::null_mut(),
+					error.as_ptr() as *mut gl::types::GLchar
+				);
+			}
+			
+			return Err(error.to_string_lossy().into_owned());
+		}
+		
+		for shader in shaders {
+			unsafe { gl::DetachShader(program_id, shader.id()); }
+		}
+		
+		Ok(Program { id: program_id })
+	}
+	
+	pub fn id(&self) -> gl::types::GLuint {
+		self.id
+	}
+	
+	pub fn set_used(&self) {
+		unsafe {
+			gl::UseProgram(self.id);
+		}
+	}
+	
+	pub fn uniform_location(&self, uniform_name: &str) -> i32 {
+		let vstr = uniform_name.as_bytes().to_owned();
+		let cstr = unsafe { CString::from_vec_unchecked(vstr) };
+		unsafe {
+			let loc = gl::GetUniformLocation(self.id, cstr.as_ptr());
+			
+			if loc == -1 {
+				eprintln!("Uniform location for '{}' is invalid.", uniform_name);
+				return 0;
+			}
+			
+			loc
+		}
+	}
+	
+	pub fn uniform_scalar(&self, uniform: i32, value: f32) {
+		if uniform == -1 {return}
+		unsafe {
+			gl::Uniform1f(uniform, value)
+		}
+	}
+	
+	pub fn uniform_vector4(&self, uniform: i32, vector: cgmath::Vector4<f32>) {
+		if uniform == -1 {return}
+		unsafe {
+			gl::Uniform4fv(uniform, 1, vector.as_ptr())
+		}
+	}
+	
+	pub fn uniform_matrix4(&self, uniform: i32, matrix: cgmath::Matrix4<f32>) {
+		if uniform == -1 {return}
+		unsafe {
+			gl::UniformMatrix4fv(uniform, 1, gl::FALSE, matrix.as_ptr())
+		}
+	}
 }
 
 impl Drop for Program {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteProgram(self.id);
-        }
-    }
+	fn drop(&mut self) {
+		unsafe {
+			gl::DeleteProgram(self.id);
+		}
+	}
 }
 
 pub struct Shader {
-    id: gl::types::GLuint,
+	id: gl::types::GLuint,
 }
 
 impl Shader {
-    pub fn id(&self) -> gl::types::GLuint {
-        self.id
-    }
-    pub fn from_res(
-        res: &Resources,
-        name: &str
-    ) -> Result<Shader, Error>{
-        const POSSIBLE_EXT: [(&str, gl::types::GLenum); 3] = [
-            (".vert", gl::VERTEX_SHADER),
-            (".frag", gl::FRAGMENT_SHADER),
-            (".geom", gl::GEOMETRY_SHADER),
-        ];
-
-        let shader_kind = POSSIBLE_EXT.iter()
-            .find(|&&(file_extension, _)| {
-                name.ends_with(file_extension)
-            })
-            .map(|&(_, kind)| kind)
-            .ok_or_else(|| Error::CanNotDetermineShaderTypeForResource { name: name.into() })?;
-    
-        println!("Loading shader: {} . {}", name, shader_kind);
-        let source = res.load_cstring(name)
-            .map_err(|e| Error::ResourceLoad { name: name.into(), inner: e })?;
-    
-        println!("Compiling shader: {}", name);
-        Shader::from_source(&source, shader_kind)
-            .map_err(|message| Error::CompileError { name: name.into(), message })
-    }
-
-    pub fn from_source(
-        source: &CStr,
-        kind: gl::types::GLenum
-    ) -> Result<Shader, String> {
-        let id = shader_from_source(source, kind)?;
-        Ok(Shader {id})
-    }
-
-    pub fn from_vert_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::VERTEX_SHADER)
-    }
-
-    pub fn from_frag_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::FRAGMENT_SHADER)
-    }
+	pub fn id(&self) -> gl::types::GLuint {
+		self.id
+	}
+	pub fn from_res(
+		res: &Resources,
+		name: &str
+	) -> Result<Shader, Error>{
+		const POSSIBLE_EXT: [(&str, gl::types::GLenum); 3] = [
+			(".vert", gl::VERTEX_SHADER),
+			(".frag", gl::FRAGMENT_SHADER),
+			(".geom", gl::GEOMETRY_SHADER),
+		];
+		
+		let shader_kind = POSSIBLE_EXT.iter()
+			.find(|&&(file_extension, _)| {
+				name.ends_with(file_extension)
+			})
+			.map(|&(_, kind)| kind)
+			.ok_or_else(|| Error::CanNotDetermineShaderTypeForResource { name: name.into() })?;
+		
+		println!("Loading shader: {} . {}", name, shader_kind);
+		let source = res.load_cstring(name)
+			.map_err(|e| Error::ResourceLoad { name: name.into(), inner: e })?;
+		
+		println!("Compiling shader: {}", name);
+		Shader::from_source(&source, shader_kind)
+			.map_err(|message| Error::CompileError { name: name.into(), message })
+	}
+	
+	pub fn from_source(
+		source: &CStr,
+		kind: gl::types::GLenum
+	) -> Result<Shader, String> {
+		let id = shader_from_source(source, kind)?;
+		Ok(Shader {id})
+	}
+	
+	pub fn from_vert_source(source: &CStr) -> Result<Shader, String> {
+		Shader::from_source(source, gl::VERTEX_SHADER)
+	}
+	
+	pub fn from_frag_source(source: &CStr) -> Result<Shader, String> {
+		Shader::from_source(source, gl::FRAGMENT_SHADER)
+	}
 }
 
 impl Drop for Shader {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteShader(self.id);
-        }
-    }
+	fn drop(&mut self) {
+		unsafe {
+			gl::DeleteShader(self.id);
+		}
+	}
 }
 
 fn shader_from_source(
-    source: &CStr,
-    kind: gl::types::GLuint
+	source: &CStr,
+	kind: gl::types::GLuint
 ) -> Result<gl::types::GLuint, String> {
-    let id = unsafe { gl::CreateShader(kind) };
-
-    unsafe {
-        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
-        gl::CompileShader(id);
-    }
-
-    let mut success: gl::types::GLint = 1;
-    unsafe {
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-    }
-
-    if success == 0 {
-        let mut len: gl::types::GLint = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-        }
-
-        let error = create_whitespace_cstring_with_len(len as usize);
-        unsafe {
-            gl::GetShaderInfoLog(
-                id, len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar
-            );
-        }
-
-        return Err(error.to_string_lossy().into_owned());
-    }
-
-    Ok(id)
+	let id = unsafe { gl::CreateShader(kind) };
+	
+	unsafe {
+		gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+		gl::CompileShader(id);
+	}
+	
+	let mut success: gl::types::GLint = 1;
+	unsafe {
+		gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+	}
+	
+	if success == 0 {
+		let mut len: gl::types::GLint = 0;
+		unsafe {
+			gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+		}
+		
+		let error = create_whitespace_cstring_with_len(len as usize);
+		unsafe {
+			gl::GetShaderInfoLog(
+				id, len,
+				std::ptr::null_mut(),
+				error.as_ptr() as *mut gl::types::GLchar
+			);
+		}
+		
+		return Err(error.to_string_lossy().into_owned());
+	}
+	
+	Ok(id)
 }
 
 fn create_whitespace_cstring_with_len(len: usize) -> CString {
-    let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
-    buffer.extend([b' '].iter().cycle().take(len as usize));
-    unsafe { CString::from_vec_unchecked(buffer) }
+	let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
+	buffer.extend([b' '].iter().cycle().take(len as usize));
+	unsafe { CString::from_vec_unchecked(buffer) }
 }
 
 pub struct Texture {
 	id: gl::types::GLuint,
+	width: u32,
+	height: u32,
+	tx: f32,
+	ty: f32,
 }
 
 impl Texture {
-
-    pub fn from_res(res: &Resources, name: &str) -> Result<Texture, Error> {
-        // TODO: The following is rather horrible code. Causes way too many copies.
-        
-        let buffer = res.load_buffer(name)
-            .map_err(|e| Error::ResourceLoad { name: name.into(), inner: e })?;
-        
-        let image = image::load_from_memory(&buffer)
-            .map_err(|e| Error::ImageParse { name: name.into(), inner: e })?;
-        
-        let image = image.as_rgba8()
-            .expect("Failed to convert Image to RGBA8.");
-        
-        let image_size = image.dimensions();
-        let image_width = image_size.0;
-        let image_height = image_size.1;
-        let image_area = image_width * image_height;
-        
-        let mut handle: gl::types::GLuint = 0;
-        unsafe {
-            // preparing
-            gl::GenTextures(1, &mut handle);
-            gl::BindTexture(gl::TEXTURE_2D, handle);
-            
-            // wrapping
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
-            
-            // sampling
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
-            
-            // uploading
-            gl::TexImage2D(gl::TEXTURE_2D,
-                0, gl::RGB as i32,
-                image_width as i32, image_height as i32,
-                0, gl::RGB, gl::UNSIGNED_BYTE,
-                image.as_ptr() as *const std::ffi::c_void
-            );
-        }
-        
-        Ok(Texture{
-            id: handle
-        })
-    }
-
+	
+	pub fn from_res(res: &Resources, name: &str) -> Result<Texture, Error> {
+		// TODO: The following is rather horrible code. Causes way too many copies.
+		
+		let buffer = res.load_buffer(name)
+			.map_err(|e| Error::ResourceLoad { name: name.into(), inner: e })?;
+		
+		let image = image::load_from_memory(&buffer)
+			.map_err(|e| Error::ImageParse { name: name.into(), inner: e })?;
+		
+		let image = image.as_rgba8()
+			.expect("Failed to convert Image to RGBA8.");
+		
+		let image_size = image.dimensions();
+		let image_width = image_size.0;
+		let image_height = image_size.1;
+		let image_area = image_width * image_height;
+		
+		let mut handle: gl::types::GLuint = 0;
+		unsafe {
+			// preparing
+			gl::GenTextures(1, &mut handle);
+			gl::BindTexture(gl::TEXTURE_2D, handle);
+			
+			// wrapping
+			gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+			gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
+			
+			// sampling
+			gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+			gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
+			
+			// uploading
+			gl::TexImage2D(gl::TEXTURE_2D,
+			               0, gl::RGB as i32,
+			               image_width as i32, image_height as i32,
+			               0, gl::RGB, gl::UNSIGNED_BYTE,
+			               image.as_ptr() as *const std::ffi::c_void
+			);
+		}
+		
+		Ok(Texture{
+			id: handle,
+			width: image_width,
+			height: image_height,
+			tx: 1.0 / image_width as f32,
+			ty: 1.0 / image_height as f32
+		})
+	}
+	
+	pub fn get_uv_rect(&self, x: u32, y:u32, w: u32, h: u32) -> [f32;4] {
+		return [
+			x as f32 * self.tx,
+			y as f32 * self.ty,
+			(x+w) as f32 * self.tx,
+			(h+w) as f32 * self.tx
+		]
+	}
+	
 }
