2d53353bcff5694f3a6c7a90f010f7f74f9d2008
Longor1996 <lalongok@gmail.com>
2019-03-17 14:48:43 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #[macro_use] extern crate failure;
+extern crate core;
+
 pub const MAGIC : u32 = 42;
 
 pub mod resources;
 pub mod gameloop;
+pub mod router;
 pub mod blocks;
 pub mod client;
 pub mod server;
