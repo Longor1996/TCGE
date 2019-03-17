a350025e92a0e3cb55fc7e6716d618d8a9efb05a
Longor1996 <lalongok@gmail.com>
2019-03-17 15:21:25 +0100

    Remove unused imports

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,14 +4,14 @@ use failure::Fail;
 
 extern crate tcge;
 use tcge::resources::Resources;
-use tcge::util::utf8;
 use tcge::router;
-//use tcge::util::cdml;
 
 fn main() {
     let res = Resources::from_exe_path().unwrap();
     
     /*
+    use tcge::util::utf8;
+    use tcge::util::cdml;
     println!("Hello, Server! {}", tcge::MAGIC);
     
     let reader = res.open_stream("test.cdml").unwrap();
@@ -22,9 +22,9 @@ fn main() {
         print!("{}", x)
     }
     println!();
-    */
     
     // let mut cdmlReader = cdml::CDMLReader::read(reader);
+    */
     
     let mut router = router::new_router();
     
