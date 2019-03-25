7497a4cdf8a67b8325db57e3cae9d39bf428121b
Longor1996 <lalongok@gmail.com>
2019-03-25 19:59:23 +0100

    Fix indentation for a few files.
    Turns out I forgot to enable automatic indentation in IntelliJ...

--- src/gameloop.rs
+++ src/gameloop.rs
@@ -1,94 +1,94 @@
 
 pub struct GameloopState {
-    //ticks_per_second: i32,
-    skip_ticks: f64,
-    max_frameskip: i32,
-    next_game_tick: f64,
-    loops: i32,
-    interpolation: f64,
-    frame_time: f64,
-    frame_count: usize,
-    tick_count: i32,
-    last_chk: f64,
-    last_fps: f64,
-    last_tps: f64,
-    print_timers: bool,
+	//ticks_per_second: i32,
+	skip_ticks: f64,
+	max_frameskip: i32,
+	next_game_tick: f64,
+	loops: i32,
+	interpolation: f64,
+	frame_time: f64,
+	frame_count: usize,
+	tick_count: i32,
+	last_chk: f64,
+	last_fps: f64,
+	last_tps: f64,
+	print_timers: bool,
 }
 
 impl GameloopState {
-    pub fn new(ticks_per_second: i32, print_timers: bool) -> GameloopState {
-        GameloopState {
-            skip_ticks: 1.0 / (ticks_per_second as f64),
-            max_frameskip: 5,
-            loops: 0,
-            tick_count: 0,
-            next_game_tick: 0.0,
-            interpolation: 0.0,
-            frame_time: 0.0,
-            frame_count: 0,
-            last_chk: 0.0,
-            last_fps: 0.0,
-            last_tps: 0.0,
-            print_timers,
-        }
-    }
-    
-    pub fn next<GTC, GT, GD> (
-        &mut self,
-        gtc: GTC,
-        mut game_tick: GT,
-        mut game_draw: GD
-    ) where
-        GTC: Fn() -> f64,
-        GT: FnMut(f64),
-        GD: FnMut(f64, f32),
-    {
-        let frame_start = gtc();
-        self.loops = 0;
-        
-        while (gtc() > self.next_game_tick) && (self.loops < self.max_frameskip) {
-            game_tick(gtc());
-    
-            self.next_game_tick += self.skip_ticks;
-            self.loops += 1;
-            self.tick_count += 1;
-        }
-        
-        let now = gtc();
-        let delta = now - self.next_game_tick;
-    
-        self.interpolation = (delta + self.skip_ticks) / self.skip_ticks;
-        game_draw(now, self.interpolation as f32);
-        
-        let frame_end = gtc();
-        self.frame_time = frame_end - frame_start;
-        self.frame_count += 1;
-        
-        if frame_end - self.last_chk > 1.0 && self.frame_count > 10 {
-            // calculate averages
-            self.last_fps = self.frame_count as f64 / (frame_end - self.last_chk);
-            self.last_tps = self.tick_count as f64 / (frame_end - self.last_chk);
-            
-            // reset timer
-            self.frame_count = 0;
-            self.tick_count = 0;
-            self.last_chk = frame_end;
-            
-            if self.print_timers {
-                debug!("{} FPS, {} TPS", self.last_fps, self.last_tps);
-            }
-        }
-    }
-    
-    pub fn get_frame_time(&self) -> f64 {
-        self.frame_time
-    }
-    
-    pub fn get_frames_per_second(&self) -> f64 {
-        self.last_fps
-    }
-    
-    pub fn get_ticks_per_second(&self) -> f64 {
-        self.last_tps
-    }
+	pub fn new(ticks_per_second: i32, print_timers: bool) -> GameloopState {
+		GameloopState {
+			skip_ticks: 1.0 / (ticks_per_second as f64),
+			max_frameskip: 5,
+			loops: 0,
+			tick_count: 0,
+			next_game_tick: 0.0,
+			interpolation: 0.0,
+			frame_time: 0.0,
+			frame_count: 0,
+			last_chk: 0.0,
+			last_fps: 0.0,
+			last_tps: 0.0,
+			print_timers,
+		}
+	}
+	
+	pub fn next<GTC, GT, GD> (
+		&mut self,
+		gtc: GTC,
+		mut game_tick: GT,
+		mut game_draw: GD
+	) where
+		GTC: Fn() -> f64,
+		GT: FnMut(f64),
+		GD: FnMut(f64, f32),
+	{
+		let frame_start = gtc();
+		self.loops = 0;
+		
+		while (gtc() > self.next_game_tick) && (self.loops < self.max_frameskip) {
+			game_tick(gtc());
+			
+			self.next_game_tick += self.skip_ticks;
+			self.loops += 1;
+			self.tick_count += 1;
+		}
+		
+		let now = gtc();
+		let delta = now - self.next_game_tick;
+		
+		self.interpolation = (delta + self.skip_ticks) / self.skip_ticks;
+		game_draw(now, self.interpolation as f32);
+		
+		let frame_end = gtc();
+		self.frame_time = frame_end - frame_start;
+		self.frame_count += 1;
+		
+		if frame_end - self.last_chk > 1.0 && self.frame_count > 10 {
+			// calculate averages
+			self.last_fps = self.frame_count as f64 / (frame_end - self.last_chk);
+			self.last_tps = self.tick_count as f64 / (frame_end - self.last_chk);
+			
+			// reset timer
+			self.frame_count = 0;
+			self.tick_count = 0;
+			self.last_chk = frame_end;
+			
+			if self.print_timers {
+				debug!("{} FPS, {} TPS", self.last_fps, self.last_tps);
+			}
+		}
+	}
+	
+	pub fn get_frame_time(&self) -> f64 {
+		self.frame_time
+	}
+	
+	pub fn get_frames_per_second(&self) -> f64 {
+		self.last_fps
+	}
+	
+	pub fn get_ticks_per_second(&self) -> f64 {
+		self.last_tps
+	}
 }
--- src/util/utf8.rs
+++ src/util/utf8.rs
@@ -4,156 +4,156 @@ use std::io::Read;
 use std::io::Error;
 
 pub struct UTF8Read<R> {
-    input: R,
-    now: char,
-    last: char,
-    nowpos: FilePosition,
-    lastpos: FilePosition,
-    unread: bool
+	input: R,
+	now: char,
+	last: char,
+	nowpos: FilePosition,
+	lastpos: FilePosition,
+	unread: bool
 }
 
 #[derive(Debug, Copy, Clone)]
 pub struct FilePosition {
-    row: usize,
-    col: usize,
-    idx: usize,
+	row: usize,
+	col: usize,
+	idx: usize,
 }
 
 #[derive(Debug)]
 pub enum UTF8ReadError {
-    Io(Error),
-    Invalid(FilePosition),
-    EOF(FilePosition),
-    DblUnread,
+	Io(Error),
+	Invalid(FilePosition),
+	EOF(FilePosition),
+	DblUnread,
 }
 
 impl<R: Read> UTF8Read<R> {
-    pub fn new(input: R) -> UTF8Read<R> {
-        UTF8Read {
-            input,
-            last: 0 as char,
-            lastpos: self::FilePosition {
-                row: 0,
-                col: 0,
-                idx: 0
-            },
-            now: 0 as char,
-            nowpos: self::FilePosition {
-                row: 0,
-                col: 0,
-                idx: 0
-            },
-            unread: false
-        }
-    }
-
-    fn onebyte(&mut self) -> Result<u8, UTF8ReadError> {
-        let mut b0 = [0];
-        if self.input.read(&mut b0)? == 1 {
-            return Result::Ok(b0[0])
-        }
-        Result::Err(UTF8ReadError::EOF(self.nowpos))
-    }
-
-    pub fn read(&mut self) -> Result<char, UTF8ReadError> {
-        if self.unread {
-            self.unread = false;
-            return Ok(self.now);
-        }
-
-        // make 'now' the new 'last'
-        self.last = self.now;
-        self.lastpos = self.nowpos;
-
-        let mut buf: [u8; 4] = [0, 0, 0, 0];
-
-        buf[0] = self.onebyte()?;
-
-        if buf[0] < 0b10000000 {
-            return Ok(buf[0] as char)
-        }
-
-        if (buf[0] & 0xC0) == 0x80 {
-            return Err(UTF8ReadError::Invalid(self.nowpos))
-        }
-
-        if (buf[0] & 0xFE) == 0xFE {
-            return Err(UTF8ReadError::Invalid(self.nowpos))
-        }
-
-        let mut result = buf[0] as u32;
-
-        let mut sequence_length = 1;
-        loop {
-            if (buf[0] & (0x80 >> sequence_length)) == 0 {
-                break
-            }
-            sequence_length += 1;
-        }
-
-        let mask = match sequence_length {
-            1 => 0b1111111,
-            2 => 0b11111,
-            3 => 0b1111,
-            4 => 0b111,
-            _ => return Err(UTF8ReadError::Invalid(self.nowpos))
-        };
-
-        result &= mask;
-
-        let mut index = 1;
-        loop {
-            if index < sequence_length {
-                break
-            }
-            buf[index] = self.onebyte()?;
-            result = (result << 6) | ((buf[index] as u32) & 0b111111);
-            index += 1;
-        }
-
-        match std::char::from_u32(result) {
-            Some(x) => {
-                self.now = x;
-
-                // update position
-                self.nowpos.idx += 1;
-                if x == '\n' {
-                    self.nowpos.row += 1;
-                    self.nowpos.col = 1;
-                } else {
-                    self.nowpos.col += 1;
-                }
-
-                Ok(x)
-            },
-            None => Err(UTF8ReadError::Invalid(self.nowpos))
-        }
-    }
-
-    pub fn unread(&mut self) -> Result<(),UTF8ReadError> {
-        if self.unread {
-            return Err(UTF8ReadError::DblUnread);
-        }
-
-        self.unread = true;
-        Ok(())
-    }
+	pub fn new(input: R) -> UTF8Read<R> {
+		UTF8Read {
+			input,
+			last: 0 as char,
+			lastpos: self::FilePosition {
+				row: 0,
+				col: 0,
+				idx: 0
+			},
+			now: 0 as char,
+			nowpos: self::FilePosition {
+				row: 0,
+				col: 0,
+				idx: 0
+			},
+			unread: false
+		}
+	}
+	
+	fn onebyte(&mut self) -> Result<u8, UTF8ReadError> {
+		let mut b0 = [0];
+		if self.input.read(&mut b0)? == 1 {
+			return Result::Ok(b0[0])
+		}
+		Result::Err(UTF8ReadError::EOF(self.nowpos))
+	}
+	
+	pub fn read(&mut self) -> Result<char, UTF8ReadError> {
+		if self.unread {
+			self.unread = false;
+			return Ok(self.now);
+		}
+		
+		// make 'now' the new 'last'
+		self.last = self.now;
+		self.lastpos = self.nowpos;
+		
+		let mut buf: [u8; 4] = [0, 0, 0, 0];
+		
+		buf[0] = self.onebyte()?;
+		
+		if buf[0] < 0b10000000 {
+			return Ok(buf[0] as char)
+		}
+		
+		if (buf[0] & 0xC0) == 0x80 {
+			return Err(UTF8ReadError::Invalid(self.nowpos))
+		}
+		
+		if (buf[0] & 0xFE) == 0xFE {
+			return Err(UTF8ReadError::Invalid(self.nowpos))
+		}
+		
+		let mut result = buf[0] as u32;
+		
+		let mut sequence_length = 1;
+		loop {
+			if (buf[0] & (0x80 >> sequence_length)) == 0 {
+				break
+			}
+			sequence_length += 1;
+		}
+		
+		let mask = match sequence_length {
+			1 => 0b1111111,
+			2 => 0b11111,
+			3 => 0b1111,
+			4 => 0b111,
+			_ => return Err(UTF8ReadError::Invalid(self.nowpos))
+		};
+		
+		result &= mask;
+		
+		let mut index = 1;
+		loop {
+			if index < sequence_length {
+				break
+			}
+			buf[index] = self.onebyte()?;
+			result = (result << 6) | ((buf[index] as u32) & 0b111111);
+			index += 1;
+		}
+		
+		match std::char::from_u32(result) {
+			Some(x) => {
+				self.now = x;
+				
+				// update position
+				self.nowpos.idx += 1;
+				if x == '\n' {
+					self.nowpos.row += 1;
+					self.nowpos.col = 1;
+				} else {
+					self.nowpos.col += 1;
+				}
+				
+				Ok(x)
+			},
+			None => Err(UTF8ReadError::Invalid(self.nowpos))
+		}
+	}
+	
+	pub fn unread(&mut self) -> Result<(),UTF8ReadError> {
+		if self.unread {
+			return Err(UTF8ReadError::DblUnread);
+		}
+		
+		self.unread = true;
+		Ok(())
+	}
 }
 
 impl<R> fmt::Debug for UTF8Read<R> where R: fmt::Debug {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        fmt.debug_struct("BufReader")
-            .field("reader", &self.input)
-            .field("now", &self.now)
-            .field("last", &self.last)
-            .field("now_pos", &self.nowpos)
-            .field("last_pos", &self.lastpos)
-            .finish()
-    }
+	fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+		fmt.debug_struct("BufReader")
+			.field("reader", &self.input)
+			.field("now", &self.now)
+			.field("last", &self.last)
+			.field("now_pos", &self.nowpos)
+			.field("last_pos", &self.lastpos)
+			.finish()
+	}
 }
 
 impl From<Error> for UTF8ReadError {
-    fn from(error: Error) -> UTF8ReadError {
-        UTF8ReadError::Io(error)
-    }
+	fn from(error: Error) -> UTF8ReadError {
+		UTF8ReadError::Io(error)
+	}
 }
