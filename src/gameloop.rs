63056bb12c19e0dd8cfb924b4c1f040264975238
Longor1996 <lalongok@gmail.com>
2019-03-31 16:37:48 +0200

    Add documentation to the gameloop module.

--- src/gameloop.rs
+++ src/gameloop.rs
@@ -1,23 +1,56 @@
-
+/// Represents a gameloop; contains the necessary state.
 pub struct GameloopState {
-	//ticks_per_second: i32,
+	/// The normal duration between individual ticks.
 	skip_ticks: f64,
+	
+	/// How many frames can be skipped when catching up with lost ticks.
 	max_frameskip: i32,
+	
+	/// The moment when the next tick happens.
 	next_game_tick: f64,
+	
+	/// Counter of catch-up ticks per frame.
 	loops: i32,
+	
+	/// The interpolation factor of the last frame.
 	interpolation: f64,
+	
+	/// The measured length in seconds of the last frame on the CPU side.
 	frame_time: f64,
+	
+	/// The x-per-second counter for frames.
 	frame_count: usize,
+	
+	/// The x-per-second counter for ticks.
 	tick_count: i32,
+	
+	/// Total number of frames the gameloop went trough.
 	total_frames: u64,
+	
+	/// Total number of ticks the gameloop went trough.
 	total_ticks: u64,
+	
+	/// The time when the last x-per-second measurements where done.
 	last_chk: f64,
+	
+	/// The last measured average frames-per-second.
 	last_fps: f64,
+	
+	/// The last measured average ticks-per-second.
 	last_tps: f64,
+	
+	/// Whether to log timing information once a second.
 	print_timers: bool,
 }
 
 impl GameloopState {
+	/// Creates a new gameloop.
+	///
+	/// # Parameters
+	///
+	/// `ticks_per_second`: The amount of 'ticks' per second the gameloop should attempt to run.
+	///
+	/// `print_timers`: If true, the gameloop will print timing information to the console/terminal every once a second.
 	pub fn new(ticks_per_second: i32, print_timers: bool) -> GameloopState {
 		GameloopState {
 			skip_ticks: 1.0 / (ticks_per_second as f64),
@@ -37,6 +70,22 @@ impl GameloopState {
 		}
 	}
 	
+	// TODO: Make this function a iterator.
+	// ALT: Make this function a generator (once possible).
+	
+	/// Applies any number of ticks trough the tick-callback and then calls the draw-callback once.
+	/// This function requires a 'timer'-closure yielding high-resolution time in seconds.
+	///
+	/// # Parameters
+	///
+	/// `gtc`: A closure that returns time as seconds in a high-resolution.
+	///
+	/// `game_tick`: The closure to call when a tick occurs;
+	/// receives the current time in seconds.
+	///
+	/// `game_draw`: The closure to call when a frame occurs;
+	/// receives the current time in seconds and the current interpolation factor.
+	/// This closure is called *once* for *every* call to this function.
 	pub fn next<GTC, GT, GD> (
 		&mut self,
 		gtc: GTC,
@@ -86,22 +135,27 @@ impl GameloopState {
 		}
 	}
 	
+	/// Returns the total number of ticks since the creation of the gameloop instance.
 	pub fn get_total_ticks(&self) -> u64 {
 		self.total_ticks
 	}
 	
+	/// Returns the total number of frames since the creation of the gameloop instance.
 	pub fn get_total_frames(&self) -> u64 {
 		self.total_frames
 	}
 	
+	/// Gets the time (in seconds) of the previous frame.
 	pub fn get_frame_time(&self) -> f64 {
 		self.frame_time
 	}
 	
+	/// Gets the average rate of frames-per-second.
 	pub fn get_frames_per_second(&self) -> f64 {
 		self.last_fps
 	}
 	
+	/// Gets the average rate of ticks-per-second.
 	pub fn get_ticks_per_second(&self) -> f64 {
 		self.last_tps
 	}
