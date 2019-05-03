0874fd99fd630258e5dab492ef245d1ecf00f3b4
Longor1996 <lalongok@gmail.com>
2019-05-03 13:19:06 +0200

    Create empty stub-file for server.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -7,28 +7,32 @@ extern crate failure;
 use failure::Fail;
 
 extern crate tcge;
-use tcge::resources::Resources;
+use tcge::resources;
+use tcge::router;
+use tcge::util::gameloop;
+use tcge::server;
 
 fn main() {
-    use simplelog::*;
-    use std::fs::File;
-    let current_exe = std::env::current_exe().expect("Failed to get path of the 'server' executable.");
-    let current_dir = current_exe.parent().expect("Failed to get path of the 'server' executables parent directory.");
-    let log_file = current_dir.join("server.log");
-    
-    let mut log_config = Config::default();
-    log_config.time_format = Some("[%Y-%m-%d %H:%M:%S]");
-    
-    println!("[HINT] Log file location: {}", log_file.to_str().unwrap_or("ERROR"));
-    CombinedLogger::init(
-        vec![
-            TermLogger::new(LevelFilter::Trace, log_config).expect("Failed to set up TermLogger for server."),
-            WriteLogger::new(LevelFilter::Info, log_config, File::create(log_file).expect("Failed to set up FileLogger for server.")),
-        ]
-    ).unwrap();
-    info!("Server startup...");
-    
-    let _res = Resources::from_exe_path().expect("Failed to setup root resource provider for server.");
-    
-    info!("Server shutdown!");
+	use simplelog::*;
+	use std::fs::File;
+	let current_exe = std::env::current_exe().expect("Failed to get path of the 'server' executable.");
+	let current_dir = current_exe.parent().expect("Failed to get path of the 'server' executables parent directory.");
+	let log_file = current_dir.join("server.log");
+	
+	let mut log_config = Config::default();
+	log_config.time_format = Some("[%Y-%m-%d %H:%M:%S]");
+	
+	println!("[HINT] Log file location: {}", log_file.to_str().unwrap_or("ERROR"));
+	CombinedLogger::init(
+		vec![
+			TermLogger::new(LevelFilter::Trace, log_config).expect("Failed to set up TermLogger for server."),
+			WriteLogger::new(LevelFilter::Info, log_config, File::create(log_file).expect("Failed to set up FileLogger for server.")),
+		]
+	).unwrap();
+	info!("Server startup...");
+	
+	let _res = resources::Resources::from_exe_path()
+		.expect("Failed to setup root resource provider for server.");
+	
+	info!("Server shutdown!");
 }
---
+++ src/server/mod.rs
@@ -0,0 +1,3 @@
+//! All the necessary code to deal with all things the game-server needs.
+
+// TODO: This is an empty stub-file for now, until development on networking starts.
