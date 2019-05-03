08d96fb1f2957605e9d5658ed2bba25a803096f3
Longor1996 <lalongok@gmail.com>
2019-05-03 23:11:04 +0200

    Add function to get time in nanoseconds

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,3 +2,11 @@
 
 pub mod utf8;
 pub mod gameloop;
+
+pub fn current_time_nanos() -> u128 {
+	use std::time::{SystemTime, UNIX_EPOCH};
+	let start = SystemTime::now();
+	let since_the_epoch = start.duration_since(UNIX_EPOCH)
+		.expect("Time went backwards");
+	return since_the_epoch.as_nanos();
+}
