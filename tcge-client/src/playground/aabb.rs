5edf90953f38e862ccf6843a335231423f1357f1
Longor1996 <lalongok@gmail.com>
2020-02-24 19:52:14 +0100

    Attempt to implement another physics algorithm

--- tcge-client/src/playground/aabb.rs
+++ tcge-client/src/playground/aabb.rs
@@ -139,6 +139,82 @@ impl AxisAlignedBoundingBox {
 		let d = self.distance_squared(other);
 		if d == 0.0 {0.0} else {d.sqrt()}
 	}
+	
+	pub fn minkowsky_diff(&self, other: &Self) -> Self {
+		let sd = self.dimensions();
+		let od = self.dimensions();
+		Self {
+			x_min: other.x_min - self.x_min - sd.x,
+			y_min: other.y_min - self.y_min - sd.y,
+			z_min: other.z_min - self.z_min - sd.z,
+			x_max: sd.x + od.x,
+			y_max: sd.y + od.y,
+			z_max: sd.z + od.z,
+		}
+	}
+	
+	pub fn segment_intersection_indices(&self, start: Vector3<f32>, end: Vector3<f32>, ti1: Option<f32>, ti2: Option<f32>) -> Option<(f32, f32, Vector3<f32>, Vector3<f32>)> {
+		
+		let mut ti1 = ti1.unwrap_or(0.0);
+		let mut ti2 = ti2.unwrap_or(1.0);
+		
+		let d: Vector3<f32> = end - start;
+		
+		let mut n1 = Vector3::new(0.0, 0.0, 0.0);
+		let mut n2 = Vector3::new(0.0, 0.0, 0.0);
+		
+		for side in 0..6 {
+			let (nx, ny, nz, p, q) = match side {
+				0 => {
+					(-1.0, 0.0, 0.0, -d.x, start.x - self.x_min)
+				},
+				1 => {
+					(1.0, 0.0, 0.0, d.x, self.x_max - start.x)
+				},
+				2 => {
+					(0.0, -1.0, 0.0, -d.y, start.y - self.y_min)
+				},
+				3 => {
+					(0.0, 1.0, 0.0, d.y, self.y_max - start.y)
+				},
+				4 => {
+					(0.0, 0.0, -1.0, -d.z, start.z - self.z_min)
+				},
+				5 => {
+					(0.0, 0.0, 1.0, d.z, self.z_max - start.z)
+				},
+				_ => panic!()
+			};
+			
+			if p == 0.0 {
+				if q <= 0.0 {
+					return None;
+				}
+			} else {
+				let r = q / p;
+				if p < 0.0 {
+					// p < 0.0
+					if r > ti2 {
+						return None;
+					} else if r > ti1 {
+						ti1 = r;
+						n1 = Vector3::new(nx, ny, nz);
+					}
+				} else {
+					// p > 0.0
+					if r < ti1 {
+						return None;
+					} else if r < ti2 {
+						ti2 = r;
+						n2 = Vector3::new(nx, ny, nz);
+					}
+				}
+			}
+		}
+		
+		Some((ti1, ti2, n1, n2))
+	}
+	
 }
 
 impl AxisAlignedBoundingBox {
@@ -231,6 +307,259 @@ impl AxisAlignedBoundingBox {
 		} else {
 			delta
 		}
+	}
+}
+
+impl AxisAlignedBoundingBox {
+	pub fn sweep_self(a: &Self, av: &cgmath::Vector3<f32>, b: &Self) -> Option<(f32, cgmath::Vector3<f32>, cgmath::Vector3<f32>, cgmath::Vector3<f32>)> {
+		
+		// Cant sweep if already intersecting
+		if a.intersect(b) {
+			return None;
+		}
+		
+		if av.x == 0.0 && av.y == 0.0 && av.z == 0.0 {
+			return None;
+		}
+		
+		let v = av;
+		
+		// Treat b as stationary, so invert v to get relative velocity
+		let v = v.neg();
+		
+		let mut hit_time = 0.0;
+		let mut out_time = 1.0;
+		
+		let mut overlap_time = Vector3::new(0.0, 0.0, 0.0);
+		
+		//=================================
+		
+		// X axis overlap
+		if v.x < 0.0 {
+			if b.x_max < a.x_min { return None; }
+			if b.x_max > a.x_min { out_time = ((a.x_min - b.x_max) / v.x).min(out_time); }
+			
+			if a.x_max < b.x_min
+			{
+				overlap_time.x = (a.x_max - b.x_min) / v.x;
+				hit_time = overlap_time.x.max(hit_time);
+			}
+		} else if v.x > 0.0
+		{
+			if b.x_min > a.x_max { return None; }
+			if a.x_max > b.x_min { out_time = ((a.x_max - b.x_min) / v.x).min(out_time); }
+			
+			if b.x_max < a.x_min
+			{
+				overlap_time.x = (a.x_min - b.x_max) / v.x;
+				hit_time = overlap_time.x.max(hit_time);
+			}
+		}
+		
+		if hit_time > out_time { return None; }
+		
+		//=================================
+		
+		// Y axis overlap
+		if v.y < 0.0 {
+			if b.y_max < a.y_min { return None; }
+			if b.y_max > a.y_min { out_time = ((a.y_min - b.y_max) / v.y).min(out_time); }
+			
+			if a.y_max < b.y_min
+			{
+				overlap_time.y = (a.y_max - b.y_min) / v.y;
+				hit_time = overlap_time.y.max(hit_time);
+			}
+		} else if v.y > 0.0
+		{
+			if b.y_min > a.y_max { return None; }
+			if a.y_max > b.y_min { out_time = ((a.y_max - b.y_min) / v.y).min(out_time); }
+			
+			if b.y_max < a.y_min
+			{
+				overlap_time.y = (a.y_min - b.y_max) / v.y;
+				hit_time = overlap_time.y.max(hit_time);
+			}
+		}
+		
+		if hit_time > out_time { return None; }
+		
+		//=================================
+		
+		// Z axis overlap
+		if v.z < 0.0 {
+			if b.z_max < a.z_min { return None; }
+			if b.z_max > a.z_min { out_time = ((a.z_min - b.z_max) / v.z).min(out_time); }
+			
+			if a.z_max < b.z_min
+			{
+				overlap_time.z = (a.z_max - b.z_min) / v.z;
+				hit_time = overlap_time.z.max(hit_time);
+			}
+		} else if v.z > 0.0
+		{
+			if b.z_min > a.z_max { return None; }
+			if a.z_max > b.z_min { out_time = ((a.z_max - b.z_min) / v.z).min(out_time); }
+			
+			if b.z_max < a.z_min
+			{
+				overlap_time.z = (a.z_min - b.z_max) / v.z;
+				hit_time = overlap_time.z.max(hit_time);
+			}
+		}
+		
+		if hit_time > out_time { return None; }
+		
+		//=================================
+		
+		// Scale resulting velocity by normalized hit time
+		let out_velo = v.neg() * hit_time;
+		
+		let hit_norm = if overlap_time.x > overlap_time.y {
+			// y is out
+			if overlap_time.x > overlap_time.z {
+				cgmath::vec3(v.x.signum(), 0.0, 0.0)
+			} else {
+				cgmath::vec3(0.0, 0.0, v.z.signum())
+			}
+		} else {
+			// x is out
+			if overlap_time.y > overlap_time.z {
+				cgmath::vec3(0.0, v.y.signum(), 0.0)
+			} else {
+				cgmath::vec3(0.0, 0.0, v.z.signum())
+			}
+		};
+		
+		Some((hit_time, overlap_time, out_velo, hit_norm))
+	}
+}
+
+pub struct AxisAlignedBoundingBoxIntersection {
+	pub overlaps: bool,
+	pub ti: f32,
+	pub mov: Vector3<f32>,
+	pub normal: Option<Vector3<f32>>,
+	pub touch: Vector3<f32>,
+	pub distance: f32,
+}
+
+impl AxisAlignedBoundingBox {
+	pub fn detect_collision(a: &Self, b: &Self, goal: Vector3<f32>) -> Option<AxisAlignedBoundingBoxIntersection> {
+		
+		let d = goal - a.min_vec();
+		
+		let i = a.minkowsky_diff(b);
+		
+		let mut ti: Option<f32> = None;
+		let mut n: Option<Vector3<f32>> = None;
+		
+		let mut overlaps = false;
+		
+		const DELTA: f32 = 1e-10;
+		
+		if i.contains_with_delta(Vector3::new(0.0, 0.0, 0.0), DELTA) {
+			
+			let p = i.nearest_corner(Vector3::new(0.0, 0.0, 0.0));
+			
+			let adim = a.dimensions();
+			let wi = adim.x.min(p.x.abs());
+			let hi = adim.y.min(p.y.abs());
+			let di = adim.z.min(p.z.abs());
+			
+			ti = Some(wi * hi * di * -1.0);
+			overlaps = true;
+		} else {
+			//
+			
+			let si = i.segment_intersection_indices(
+				Vector3::new(0.0, 0.0, 0.0),
+				d,
+				Some(-std::f32::INFINITY),
+				Some(std::f32::INFINITY)
+			);
+			
+			if let Some((ti1, ti2, n1, n2)) = si {
+				
+				if ti1 < 1.0
+					&& ((ti1 - ti2).abs() >= DELTA)
+					&& (0.0 < ti1 + DELTA || (0.0 == ti1 && ti2 > 0.0))
+				{
+					ti = Some(ti1);
+					n = Some(n1);
+					overlaps = false;
+				}
+			}
+		}
+		
+		let ti = match ti {
+			Some(ti) => ti,
+			None => return None
+		};
+		
+		let mut t: Vector3<f32>;
+		
+		if overlaps {
+			if d.x == 0.0 && d.x == 0.0 && d.z == 0.0 {
+				// intersecting and not moving - use minimum displacement vector
+				
+				let mut p = i.nearest_corner(Vector3::new(0.0, 0.0, 0.0));
+				
+				if p.x.abs() <= p.y.abs() && p.x.abs() <= p.z.abs() {
+					p.y = 0.0;
+					p.z = 0.0;
+				} else if p.y.abs() <= p.z.abs() {
+					p.x = 0.0;
+					p.z = 0.0;
+				} else {
+					p.x = 0.0;
+					p.y = 0.0;
+				}
+				
+				n = Some(Vector3::new(p.x.signum(), p.y.signum(), p.z.signum()));
+				t = Vector3::new(
+					a.x_min + p.x,
+					a.y_min + p.y,
+					a.z_min + p.z
+				);
+			} else {
+				// intersecting and moving - move in the opposite direction
+				
+				let (ti1, _, n1, n2) = match i.segment_intersection_indices(
+					Vector3::new(0.0, 0.0, 0.0),
+					d,
+					Some(-std::f32::INFINITY),
+					Some(1.0)
+				) {
+					Some(si) => si,
+					None => return None
+				};
+				
+				// -- tunnel
+				n = Some(n1);
+				t = Vector3::new(
+					a.x_min + d.x * ti1,
+					a.y_min + d.y * ti1,
+					a.z_min + d.z * ti1
+				);
+			}
+		} else {
+			// -- tunnel
+			t = Vector3::new(
+				a.x_min + d.x * ti,
+				a.y_min + d.y * ti,
+				a.z_min + d.z * ti
+			);
+		}
+		
+		Some(AxisAlignedBoundingBoxIntersection {
+			overlaps,
+			ti,
+			mov: d,
+			normal: n,
+			touch: t,
+			distance: a.distance_squared(b)
+		})
 	}
 	
 }
